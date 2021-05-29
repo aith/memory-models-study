1eb3e06ce60a032850cc3690244c5248b5c47de3
aith <ariiramanesh@gmail.com>
2021-05-28 22:51:59 -0700

    finished part3

--- part3/RDekkers.h
+++ part3/RDekkers.h
@@ -1,22 +1,34 @@
 #include <atomic>
 
-// this should be the same as SCDekkers with the change that every
-// relaxed access should be memory_order_relaxed
+using namespace std;
 
 class dekkers_mutex {
 public:
-  dekkers_mutex() {
-    // implement me!
-  }
+    dekkers_mutex() {
+        turn.store(0, memory_order_relaxed);
+        wants_to_enter = new atomic_bool [2];
+        wants_to_enter[0] = false;
+        wants_to_enter[1] = false;
+    }
 
-  void lock(int tid) {
-    // implement me!
-  }
+    void lock(int tid) {
+        wants_to_enter[tid].store(true, memory_order_relaxed);
+        while (wants_to_enter[!tid].load(memory_order_relaxed)) {
+            if (turn.load(memory_order_relaxed) != tid) {
+                wants_to_enter[tid].store(false, memory_order_relaxed);
+                while (turn.load(memory_order_relaxed) != tid) {
+                }
+                wants_to_enter[tid].store(true, memory_order_relaxed);
+            }
+        }
+    }
 
-  void unlock(int tid) {
-    // implement me!
-  }
+    void unlock(int tid) {
+        turn.store(!tid, memory_order_relaxed);
+        wants_to_enter[tid].store(false, memory_order_relaxed);
+    }
 
 private:
-  // Give me some private variables  
+    atomic_bool *wants_to_enter;
+    atomic_int turn = 0;
 };
--- part3/TSODekkers.h
+++ part3/TSODekkers.h
@@ -5,23 +5,42 @@
 // your job is to use the following MACRO to fix the mutex.
 // Think about the reorderings allowed under TSO and where the fence
 // needs to be placed.
+using namespace std;
 
 #define FENCE asm volatile("mfence":::"memory");
 
 class dekkers_mutex {
 public:
-  dekkers_mutex() {
-    // implement me!
-  }
+    dekkers_mutex() {
 
-  void lock(int tid) {
-    // implement me!
-  }
+        turn.store(0, memory_order_relaxed);
+        wants_to_enter = new atomic_bool[2];
+        wants_to_enter[0] = false;
+        wants_to_enter[1] = false;
 
-  void unlock(int tid) {
-    // implement me!
-  }
+    }
+
+    void lock(int tid) {
+        wants_to_enter[tid].store(true, memory_order_relaxed);
+        FENCE
+        while (wants_to_enter[!tid].load(memory_order_relaxed)) {
+            if (turn.load(memory_order_relaxed) != tid) {
+                wants_to_enter[tid].store(false, memory_order_relaxed);
+                FENCE
+                while (turn.load(memory_order_relaxed) != tid) {
+                }
+                wants_to_enter[tid].store(true, memory_order_relaxed);
+                FENCE
+            }
+        }
+    }
+
+    void unlock(int tid) {
+        turn.store(!tid, memory_order_relaxed);
+        wants_to_enter[tid].store(false, memory_order_relaxed);
+    }
 
 private:
-  // Give me some private variables  
+    atomic_bool *wants_to_enter;
+    atomic_int turn = 0;
 };
