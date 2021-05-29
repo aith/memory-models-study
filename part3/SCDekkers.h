03ec23690d8eba45e57206f2deca3c04ef76514e
aith <ariiramanesh@gmail.com>
2021-05-28 22:01:33 -0700

    3.1 done

--- part3/SCDekkers.h
+++ part3/SCDekkers.h
@@ -1,19 +1,34 @@
 #include <atomic>
 
+using namespace std;
+
 class dekkers_mutex {
 public:
-  dekkers_mutex() {
-    // implement me!
-  }
+    dekkers_mutex() {
+        turn.store(0);
+        wants_to_enter = new atomic_bool [2];
+        wants_to_enter[0] = false;
+        wants_to_enter[1] = false;
+    }
 
-  void lock(int tid) {
-    // implement me!
-  }
+    void lock(int tid) {
+        wants_to_enter[tid].store(true);
+        while (wants_to_enter[!tid].load()) {
+            if (turn.load() != tid) {
+                wants_to_enter[tid].store(false);
+                while (turn.load() != tid) {
+                }
+                wants_to_enter[tid].store(true);
+            }
+        }
+    }
 
-  void unlock(int tid) {
-    // implement me!
-  }
+    void unlock(int tid) {
+        turn.store(!tid);
+        wants_to_enter[tid].store(false);
+    }
 
 private:
-  // Give me some private variables  
+    atomic_bool *wants_to_enter;
+    atomic_int turn = 0;
 };
